3f9c2d70c3bfb5b0efd069e68da5dc937e3eacdb
Léonard Hetsch <leo.hetsch@makers.tech>
2021-02-11 17:23:27 +0000

    Add heroku config bits

--- lib/kanban_liveview_web/endpoint.ex
+++ lib/kanban_liveview_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule KanbanLiveviewWeb.Endpoint do
   ]
 
   socket "/socket", KanbanLiveviewWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
